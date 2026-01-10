8cb51914c76e846cf0fd21bd7cc9d7eee6034179
Penguin60 <radean.rashed@gmail.com>
2026-01-10 13:15:45 -0500

    Add TranscriptionLine component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,13 @@
+import TranscriptionLine from "@/components/TranscriptionLine";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-        
+    <div className="min-h-screen font-sans bg-zinc-900">
+        <h1 className="text-white">Hello world</h1>
+        <div className="w-[400px] bg-black">
+          <TranscriptionLine text="This is a sample transcription line." speaker="Speaker 1" time="01:23" />
+        </div>
     </div>
   );
 }
---
+++ frontend/components/TranscriptionLine.tsx
@@ -0,0 +1,21 @@
+export default function TranscriptionLine({
+  text,
+  speaker,
+  time,
+}: {
+  text: string;
+  speaker: string;
+  time: string;
+}) {
+  return (
+    <div className="w-full flex justify-between items-center">
+      <div>
+        <h1 className="text-white">{text}</h1>
+        <h3 className="text-zinc-400 text-sm">{speaker}</h3>
+      </div>
+      <div className="bg-zinc-400 w-fit px-2 rounded-full text-sm">
+        <h3 className="text-white">{time}</h3>
+      </div>
+    </div>
+  );
+}
