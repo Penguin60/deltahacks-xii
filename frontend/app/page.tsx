e63cdf378d6c3dbf158715738162e1a10e08183f
Leo Tang <leotang634@gmail.com>
2026-01-10 13:16:37 -0500

    Layout for dashboard

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,27 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-        
-    </div>
-  );
+	return (
+		<div className="flex flex-col min-h-screen font-sans bg-zinc-900 p-5 flex-col">
+			<div className="flex flex-row w-full h-12 bg-zinc-800 p-5">
+
+            </div>
+            <div className="flex flex-row flex-1 w-full mt-3">
+                <div className="flex flex-col flex-1 bg-zinc-800 flex-[1.5]">
+                    
+                </div>
+                <div className="flex flex-col flex-1 flex-[4] mx-3">
+                    <div className="flex flex-[5] bg-zinc-800 mb-3">
+
+                    </div>
+                    <div className="flex flex-[2.5] bg-zinc-800">
+
+                    </div>
+                </div>
+                <div className="flex flex-col flex-1 bg-zinc-800 flex-[2]">
+
+                </div>
+            </div>
+		</div>
+	);
 }
